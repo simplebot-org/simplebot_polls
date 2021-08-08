323efe62d2cfbda13202296300cd4b7febc52bbb
adbenitez <asieldbenitez@gmail.com>
2021-08-08 19:00:41 -0400

    fix linter warning

--- simplebot_polls/templates/__init__.py
+++ simplebot_polls/templates/__init__.py
@@ -3,7 +3,7 @@
 from jinja2 import Environment, PackageLoader, select_autoescape
 
 env = Environment(
-    loader=PackageLoader(__name__.split(".")[0], "templates"),
+    loader=PackageLoader(__name__.split(".", maxsplit=1)[0], "templates"),
     autoescape=select_autoescape(["html", "xml"]),
 )
 template = env.get_template("poll.j2")
