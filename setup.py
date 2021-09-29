bb3b0af48f3828852dff89c0fbcf9172e3f0bc80
adbenitez <asieldbenitez@gmail.com>
2021-09-28 23:05:45 -0400

    fix linter warnings

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     DESC = "Create polls in Delta Chat (SimpleBot plugin)"
     KEYWORDS = "simplebot plugin deltachat polls"
 
-    with open("README.rst") as fh:
+    with open("README.rst", encoding="utf-8") as fh:
         long_description = fh.read()
 
     setup(
@@ -61,6 +61,6 @@ if __name__ == "__main__":
             "dev": load_requirements("requirements/requirements-dev.txt"),
         },
         entry_points={
-            "simplebot.plugins": "{0} = {0}".format(MODULE_NAME),
+            "simplebot.plugins": f"{MODULE_NAME} = {MODULE_NAME}",
         },
     )
